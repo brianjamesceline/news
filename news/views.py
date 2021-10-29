b09a1b388fb55f7692660badf465676f31ce673c
brianjamesceline <brianceline@gmail.com>
2021-10-30 00:25:13 +0100

    first commit

---
+++ news/views.py
@@ -0,0 +1,27 @@
+from django.views import generic
+from .models import Article
+
+
+class ArticleListView(generic.ListView):
+    model = Article
+
+
+class ArticleDetailView(generic.DetailView):
+    model = Article
+
+
+class ArticleCreateView(generic.CreateView):
+    model = Article
+    fields = ["pub_date", "title", "content"]
+    success_url = "/"
+
+
+class ArticleUpdateView(generic.UpdateView):
+    model = Article
+    fields = ["pub_date", "title", "content"]
+    success_url = "/"
+
+
+class ArticleDeleteView(generic.DeleteView):
+    model = Article
+    success_url = "/"
